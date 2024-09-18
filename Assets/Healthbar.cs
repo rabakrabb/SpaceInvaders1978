ffef2d55575a73fa60cb5accd4acfb8c317f674c
rabakrabb <rabadab@gmail.com>
2024-09-18 14:25:44 +0200

    Initial Commit

---
+++ Assets/Healthbar.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Image[] ships; //0, 1, 2
+
+   /* private void Start()
+    {
+        UpdateHealth();
+    }
+   */
+    public void UpdateHealth()
+    {
+        int currentHealth = 1;
+        for (int i = 0; i < ships.Length; i++)
+        {
+            Image ship = ships[i];
+            if (currentHealth > i)
+            {
+                ship.color = Color.white;
+            }
+            else
+            {
+                ship.color = Color.red;
+            }
+        }
+    }
+}
